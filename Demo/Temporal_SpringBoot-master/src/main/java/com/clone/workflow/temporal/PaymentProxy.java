73cb111d0b9ae9ec97ac0a77f27913a79288f305
Karmanya Sharma <karsharm12@publicisgroupe.net>
2022-02-09 15:18:45 +0530

    Feign changes

--- Demo/Temporal_SpringBoot-master/src/main/java/com/clone/workflow/temporal/PaymentProxy.java
+++ Demo/Temporal_SpringBoot-master/src/main/java/com/clone/workflow/temporal/PaymentProxy.java
@@ -4,7 +4,7 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-@FeignClient(name="paymentproxy", url="http://localhost:9002")
+@FeignClient(name="paymentproxy", url="http://127.0.0.1:9002")
 public interface PaymentProxy {
 	@GetMapping("/payment/{id}")
 	public String confirmPayment(@PathVariable String id);
