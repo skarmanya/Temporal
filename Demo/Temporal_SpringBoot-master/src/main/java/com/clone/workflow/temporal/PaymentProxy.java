fff8ef1fd20db3dbaa4bbe3fb83b8a938861acef
Karmanya Sharma <karsharm12@publicisgroupe.net>
2022-02-14 16:36:50 +0530

    Temporal Changes

--- Demo/Temporal_SpringBoot-master/src/main/java/com/clone/workflow/temporal/PaymentProxy.java
+++ Demo/Temporal_SpringBoot-master/src/main/java/com/clone/workflow/temporal/PaymentProxy.java
@@ -4,7 +4,7 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-@FeignClient(name="paymentproxy", url="http://ad59a5d78d7ee4a12b9c78fc81dfca93-980020954.us-east-1.elb.amazonaws.com:9002")
+@FeignClient(name="paymentproxy", url="http://172.20.138.34:9002")
 public interface PaymentProxy {
 	@GetMapping("/payment/{id}")
 	public String confirmPayment(@PathVariable String id);
