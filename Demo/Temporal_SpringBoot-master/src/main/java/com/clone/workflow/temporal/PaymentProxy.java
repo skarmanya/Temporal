e4a4e1a0082fd672c5e6e028d3b8483816f2a2e2
Karmanya Sharma <karsharm12@publicisgroupe.net>
2022-02-16 10:49:22 +0530

    Ip Changed

--- Demo/Temporal_SpringBoot-master/src/main/java/com/clone/workflow/temporal/PaymentProxy.java
+++ Demo/Temporal_SpringBoot-master/src/main/java/com/clone/workflow/temporal/PaymentProxy.java
@@ -4,7 +4,7 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-@FeignClient(name="paymentproxy", url="http://172.20.126.157:9002")
+@FeignClient(name="paymentproxy", url="http://172.20.116.114:9002")
 public interface PaymentProxy {
 	@GetMapping("/payment/{id}")
 	public String confirmPayment(@PathVariable String id);
