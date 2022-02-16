01142152c505e5a37d7c91f5ce72085ef9e30135
Karmanya Sharma <karsharm12@publicisgroupe.net>
2022-02-16 14:25:10 +0530

    Ip Changes

--- Demo/Temporal_SpringBoot-master/src/main/java/com/clone/workflow/config/TemporalConfig.java
+++ Demo/Temporal_SpringBoot-master/src/main/java/com/clone/workflow/config/TemporalConfig.java
@@ -16,7 +16,7 @@ import io.temporal.worker.WorkerFactory;
 @Configuration
 public class TemporalConfig {
 
-	private String temporalServiceAddress = "172.20.152.69:7233";
+	private String temporalServiceAddress = "172.20.47.77:7233";
 
 	private String temporalNamespace = "temporal-system";
 
